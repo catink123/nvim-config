8b9cf8a74a67ba091deb188df1add792694bb08f
catink123 <qwerty.qaz120@gmail.com>
2022-07-28 02:59:50 +0300

    Update bootstrapper script to copy the config files.

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
 echo "Welcome to Catink123's Neovim Config bootstrapper script! This script installs neccesary packages for Neovim packages to work properly."
 echo "The script may ask you to enter your root password to use the package manager."
 echo "Beginning installation..."
 echo
 echo "Installing/updating Neovim..."
 sudo apt-get update && sudo apt-get install neovim
+echo "Installing config to the correct directory..."
+if [ -d "$HOME/.config/nvim ]
+then
+  mkdir "$HOME/.config/nvim"
+fi
+cp -r . "$HOME/.config/nvim"
 echo "Installing Node.js..."
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get update && sudo apt-get install -y nodejs build-essentials 
